792588eb882701963412993ae920a2b1b43bbc18
Momchil Tomov <momchil@memsql.com>
2016-12-01 12:51:33 -0500

    causal induction

---
+++ causal.m
@@ -0,0 +1,96 @@
+% Causal induction from contingency data
+% Given a potential cause c and an effect e and the correlation between
+% them (i.e. given # of occurences of (e+|c+), (e-|c+), (e+|c-), (e-|c-),
+% infer whether c causes e.
+% 
+% Basically does a model comparison -- a bayes net that has a c->e edge vs.
+% one that does not (and only has a edge from background noise b->e).
+%
+
+
+% counts for e+c+, e-c+, e+c-, e-c-
+% Table 1 from http://cocosci.berkeley.edu/tom/papers/bayeschapter.pdf
+%
+d = [0 8 0 8];
+
+dw0 = 0.01;
+dw1 = 0.01;
+
+noisy_OR = @(b, c, w0, w1) 1 - (1 - w0)^b * (1 - w1)^c;
+% linear = @(b, c, w0, w1) w0 * b + w1 * c; % marginalization doesn't work b/c we need 0 <= w0 + w1 <= 1
+
+parametrization = noisy_OR;
+
+g0 = struct;
+g1 = struct;
+
+g0.prior = 0.5;
+g1.prior = 0.5;
+
+% P(d | w0, Graph 0) = P(e+)^(# of e+c+ and e+c-) + P(e-)^(# of e-c+ and e-c-)
+% where P(e+) = P(e+ | b+) * P(b+) + P(e+ | b-) * P(b-) from marginalization / sum rule; ditto for P(e-)
+% BUT... P(b+) = 1 and P(b-) = 0 ! (background noise is always present)
+% => P(e+) = P(e+ | b+) = w0   P(e-) = P(e- | b+) = 1 - w0
+%
+g0.P_d_given_w0 = @(d, w0) w0 ^ (d(1) + d(3)) * (1 - w0) ^ (d(2) + d(4));
+
+% P(d | w0, w1, Graph 1) = ...
+% also don't forget P(b+) = 1
+%
+g1.P_d_given_w0_w1 = @(d, w0, w1) ...
+                     parametrization(1, 1, w0, w1) ^ d(1) * ...  % e+c+
+                (1 - parametrization(1, 1, w0, w1)) ^ d(2) * ... % e-c+
+                     parametrization(1, 0, w0, w1) ^ d(3) * ...  % e+c-
+                (1 - parametrization(1, 0, w0, w1)) ^ d(4);      % e-c-
+
+
+% P(d | Graph 0) = integral P(d | w0, Graph 0) * P(Graph 0) * dw0
+g0.likelihood = @(d) sum(arrayfun(@(w0) g0.P_d_given_w0(d, w0) * g0.prior * dw0, 0:dw0:1));
+% P(d | Graph 1) = integral P(d | w0, w1, Graph 1) * P(Graph 1) * dw0 * dw1
+g1.likelihood = @(d) sum(arrayfun(@(w0) sum(arrayfun(@(w1) g1.P_d_given_w0_w1(d, w0, w1) * g1.prior * dw0 * dw1, 0:dw1:1)), 0:dw0:1));
+
+fprintf('P(d | Graph 0) = %e\n', g0.likelihood(d));
+fprintf('P(d | Graph 1) = %e\n', g1.likelihood(d));
+
+% causal support for g1 over g0
+support = @(d) log(g1.likelihood(d) / g0.likelihood(d));
+
+fprintf('support for Graph 1 over Graph 0 = %f\n', support(d));
+
+%
+% Do some plotting
+%
+
+% each row = counts for e+c+ e-c+ e+c- e-c- 
+cases = [8 0 8 0; ...
+         6 2 6 2; ...
+         4 4 4 4; ...
+         2 6 2 6; ...
+         0 8 0 8; ...
+         8 0 6 2; ...
+         6 2 4 4; ...
+         4 4 2 6; ...
+         2 6 0 8; ...
+         8 0 4 4; ...
+         6 2 2 6; ...
+         4 4 0 8; ...
+         8 0 2 6; ...
+         6 2 0 8; ...
+         8 0 0 8];
+    
+% Compute & slot causal support for each case
+% Figure 4 in http://cocosci.berkeley.edu/tom/papers/bayeschapter.pdf
+%
+s = [];
+labels = {};
+for i = 1:size(cases, 1)
+    d = cases(i, :);
+    assert(sum(d) == 16);
+    s = [s support(d)];
+    labels = [labels, sprintf('%d/%d,%d/%d', d(1), d(1) + d(2), d(3), d(3) + d(4))];
+end
+
+figure;
+bar(s - min(s) + 1);
+set(gca, 'xticklabel', labels);
+ylabel('support = P(d | Graph 1) / P(d | Graph 0)');
--- coin_model.m
+++ coin_model.m
@@ -1,8 +1,8 @@
 % Bayesian inference for coin flips: Model selection
 %
 % Two hypotheses -- 1) coin is fair, 2) coin is unfair
-% Note that h2 has multiple possible values of theta.
-% Solve by computing marginal probability of d under h2 by integrating over theta
+% Note that h1 has multiple possible values of theta.
+% Solve by computing marginal probability of d under h1 by integrating over theta
 %
 
 % Constants
@@ -20,14 +20,14 @@ disp(d)
 %
 % Hypothesis space
 %
+h0 = struct;
 h1 = struct;
-h2 = struct;
 
-h1.theta = 0.5; % candidate coin #1 is fair => specific theta
-h2.theta_prior = @(theta) 1; % candidate coin #2 is unfair; have a uniform prior for theta p(theta | h2)
+h0.theta = 0.5; % candidate coin #1 is fair => specific theta
+h1.theta_prior = @(theta) 1; % candidate coin #2 is unfair; have a uniform prior for theta p(theta | h1)
 
-h1.prior = 0.5; % prior P(h1) -- both coins are equally likely
-h2.prior = 0.5; % prior P(h2)
+h0.prior = 0.5; % prior P(h0) -- both coins are equally likely
+h1.prior = 0.5; % prior P(h1)
 
 % theta likelihood = P(data | theta)
 %
@@ -35,15 +35,15 @@ likelihood = @(d, theta) theta ^ sum(d) * (1 - theta) ^ (length(d) - sum(d));
 
 % hypothesis likelihood = P(data | h)
 %
-h1.likelihood = @(d) likelihood(d, h1.theta); % the fair coin has only 1 theta
+h0.likelihood = @(d) likelihood(d, h0.theta); % the fair coin has only 1 theta
 % the unfair coin can be any one of many thetas => we must marginalize d over
-% theta to get P(d | h2)
-h2.likelihood = @(d) sum(arrayfun(@(theta) likelihood(d, theta) * h2.theta_prior(theta) * dtheta, 0:dtheta:1));
+% theta to get P(d | h1)
+h1.likelihood = @(d) sum(arrayfun(@(theta) likelihood(d, theta) * h1.theta_prior(theta) * dtheta, 0:dtheta:1));
 
+fprintf('P(d | h0) = %e\n', h0.likelihood(d));
 fprintf('P(d | h1) = %e\n', h1.likelihood(d));
-fprintf('P(d | h2) = %e\n', h2.likelihood(d));
 
-H = {h1 h2}; % all hypotheses
+H = {h0 h1}; % all hypotheses
 
 %
 % Compute the posteriors
@@ -57,12 +57,12 @@ marginalize = @(d) sum(cellfun(@(h) h.likelihood(d) * h.prior, H));
 %
 posterior = @(h, d) h.likelihood(d) * h.prior / marginalize(d);
 
+fprintf('P(h0 | d) = %e\n', posterior(h0, d));
 fprintf('P(h1 | d) = %e\n', posterior(h1, d));
-fprintf('P(h2 | d) = %e\n', posterior(h2, d));
 
-% log posterior odds in favor of h2 (biased coin)
+% log posterior odds in favor of h1 (biased coin)
 %
-fprintf('log P(h2 | d) / P(h1 | d) = %f\n', log(posterior(h1, d) / posterior(h2, d)));
+fprintf('log P(h1 | d) / P(h0 | d) = %f\n', log(posterior(h0, d) / posterior(h1, d)));
 
 % some plotting
 % Figure 1 in http://cocosci.berkeley.edu/tom/papers/bayeschapter.pdf
@@ -79,11 +79,11 @@ for theta = thetas
             break
         end
     end
-    lpo = log(posterior(h2, d) / posterior(h1, d));
+    lpo = log(posterior(h1, d) / posterior(h0, d));
     lpos = [lpos lpo];
 end
 
 figure;
 bar(thetas, lpos);
 xlabel('\theta = P(heads)');
-ylabel('log posterior odds in favor of h2 (biased coin)');
+ylabel('log posterior odds in favor of h1 (biased coin)');
